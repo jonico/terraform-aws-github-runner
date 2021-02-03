3dbe90ab8bf8de26f43ecc878e3c04d05c959a04
zelda <zelda@users.noreply.github.com>
2021-02-03 23:58:01 +0000

    AWS changes for runner scale set in env zelda

---
+++ actions-runner-envs/zelda/terraform.tfvars
@@ -0,0 +1,10 @@
+github_app_id=45
+github_app_client_id="Iv1.c4b6936d7f01d9d3"
+github_enterprise_url="https://octodemo.com"
+instance_type="t3.micro"
+minimum_running_time_in_minutes=10
+runners_maximum_count=3
+runners_minimum_count=1
+runners_environment="zelda"
+runners_ami_owner="869946923544"
+runners_ami_filter="actions-runner-with-additional-packages-*"
---
+++ actions-runner-envs/zelda/variables.tf
@@ -0,0 +1,40 @@
+
+variable "github_app_key_base64" {
+  sensitive = true
+}
+
+variable "github_app_id" {
+}
+
+variable "github_app_client_id" {
+}
+
+variable "github_app_client_secret" {
+  sensitive = true
+}
+
+variable "github_enterprise_url" {
+}
+
+
+variable "instance_type" {
+  default="t3.micro"
+}
+
+variable "minimum_running_time_in_minutes" {
+}
+
+variable "runners_maximum_count" {
+}
+
+variable "runners_minimum_count" {
+}
+
+variable "runners_ami_owner" {
+}
+
+variable "runners_ami_filter" {
+}
+
+variable "runners_environment" {
+}
